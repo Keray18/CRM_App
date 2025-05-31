c1d200336b6758ba1cb230f1acf920442be774fa
Keray18 <keshavbahoray18@gmail.com>
2025-05-31 16:18:56 +0530

    Added the url to cors

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 // Optimized CORS configuration
 app.use(cors({
     credentials: true,
-    origin: ["https://crm-app-sandy.vercel.app/", "http://localhost:3000"],
+    origin: ["https://crm-app-sandy.vercel.app", "http://localhost:3000"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: [
