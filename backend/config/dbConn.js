b160566e143be50cdcdb70607e615cb79b552d5f
Keray18 <keshavbahoray18@gmail.com>
2025-05-31 15:26:02 +0530

    removed previous commit changes

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -25,12 +25,6 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
     logging: false,
-    pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
     define: {
         timestamps: true,
         underscored: true, // Use snake_case for column names
